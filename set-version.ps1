d3cb085a1e709d74d1dc4a5986663a8982e7ab13
Christian Specht <christi@nspecht.de>
2017-12-28 01:06:25 +0100

    set version number before build and patch assembly info

---
+++ set-version.ps1
@@ -0,0 +1,29 @@
+﻿
+# are we on AppVeyor?
+$appveyor = $env:APPVEYOR
+
+if ($appveyor){
+    
+    # set version number
+    if ($env:APPVEYOR_REPO_TAG)
+    {
+        # we are building a tag -> we are doing a release -> use the tag as version number
+        $version = $env:APPVEYOR_REPO_TAG_NAME + '.' + $env:APPVEYOR_REPO_COMMIT
+    }
+    else
+    {
+        # regular CI build, no release
+        $version = '0.0.0.CI-' + $env:APPVEYOR_BUILD_NUMBER
+    }
+
+    appveyor AddMessage $version
+
+    $env:ScmBackupVersion=$version
+
+} else {
+
+    # TODO
+}
+
+Write-Host 'GithubApiTests_Name: ' $env:GithubApiTests_Name
+Write-Host 'ScmBackupVersion: ' $env:ScmBackupVersion
